3b976640c56847d02ad4e3bf6f4310fd303735a1
jinw91 <nanhua.proarcs@gmail.com>
2018-01-14 21:17:52 -0600

    Merged changes from MoveMe LoginActivity to MoveeWorkflowActivity

--- app/src/main/java/app/moveme/moveme/LoginActivity.java
+++ app/src/main/java/app/moveme/moveme/LoginActivity.java
@@ -1,8 +1,12 @@
 package app.moveme.moveme;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * Created by Nanhua Jin on 1/14/2018.
+ */
 public class LoginActivity extends AppCompatActivity {
 
 
@@ -10,5 +14,6 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
     }
 }
---
+++ app/src/main/java/app/moveme/moveme/MoveeWorkflowActivity.java
@@ -0,0 +1,60 @@
+package app.moveme.moveme;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.NumberPicker;
+import android.widget.ViewFlipper;
+
+/**
+ * Created by Daniel on 1/14/2018.
+ */
+
+public class MoveeWorkflowActivity extends AppCompatActivity
+{
+	private static final int MAX_MOVERS = 10;
+
+	private ViewFlipper workflowVF;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_movee_workflow);
+
+		workflowVF = (ViewFlipper) findViewById(R.id.moveeVF);
+		Button nextButton = (Button) findViewById(R.id.movee_schedule_location_button_next_step);
+		Button backButton = (Button) findViewById(R.id.movee_scheduling_people_button_prev);
+		NumberPicker numMovers = findViewById(R.id.movee_people_number_picker);
+
+
+		numMovers.setMinValue(0);
+		numMovers.setValue(1);
+		numMovers.setMaxValue(MAX_MOVERS);
+
+
+		nextButton.setOnClickListener(new View.OnClickListener()
+		{
+			@Override
+			public void onClick(View view)
+			{
+				workflowVF.setInAnimation(getApplicationContext(), R.anim.transition_in_from_right);
+				workflowVF.setOutAnimation(getApplicationContext(), R.anim.transition_out_to_left);
+				workflowVF.showNext();
+			}
+		});
+
+		backButton.setOnClickListener(new View.OnClickListener()
+		{
+			@Override
+			public void onClick(View view)
+			{
+				workflowVF.setInAnimation(getApplicationContext(), R.anim.transition_in_from_left);
+				workflowVF.setOutAnimation(getApplicationContext(), R.anim.transition_out_to_right);
+				workflowVF.showPrevious();
+			}
+		});
+
+	}
+}
